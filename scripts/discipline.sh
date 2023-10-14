cbbfc6fcc8afe1660fd0c2f2304ecea853bf1fb3
Berke DURAK <bd@exhrd.fr>
2023-10-14 12:59:13 +0200

    First plausible user script

--- scripts/discipline.sh
+++ scripts/discipline.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-CFG=${CFG:-/usr/local/etc/discipline.cfg}
+CFG=${CFG:-/usr/local/etc/discipline/discipline.cfg}
 
 fail() {
     echo "$0: $*" >&2
     exit 1
+}
+
+msg() {
+    echo "-- $*"
 }
 
 if [ -e $CFG ]; then
@@ -17,12 +21,56 @@ fi
 
 t_alert_last=0
 
-$CLIENT --sender-subject $KID \
+kick() {
+    if [ $DRY_RUN = 1 ]; then
+	msg "Would be kicking $KID_USER"
+    else
+	loginctl \
+	    list-sessions -o json |
+	    jq '.[] | select(.user == "'"$KID_USER"'").session' |
+	    tr -d '"' |
+	    while read sess ; do
+		msg "Kicking $KID_USER"
+		loginctl terminate-session $sess
+	    done
+    fi
+}
+
+alert() {
+    if [ $DRY_RUN = 1 ]; then
+	msg "Would be sounding alert"
+    else
+	su $KID_USER -c "XDG_RUNTIME_DIR=/run/user/1000 aplay ${ALERT:a}"
+    fi
+}
+
+ialert=1
+
+$CLIENT --url $URL \
+	--sender-subject $KID \
+	--subject $KID \
 	--show-time-remaining \
 	--loop-delay $LOOP_DELAY \
-	--retry-delay $RETRY_DELAY | while true ; do
-    read T
-    t_now=$(date +%s)
-
+	--retry-delay $RETRY_DELAY | while read T ; do
+    if [ -z "$T" ]; then
+	msg "Empty response"
+	continue
+    fi
     
+    if [ $T = 0 ]; then
+	kick
+	continue
+    fi
+
+    do_alert=0
+    for (( i=ialert; i<=$#ALERT_THRESHOLDS; i++ )) ; do
+	(( thr=${ALERT_THRESHOLDS[ialert]} ))
+	if (( T <= thr )) ; then
+	    do_alert=1
+	    (( ialert=i+1 ))
+	fi
+    done
+    if (( do_alert != 0 )) ; then
+	alert
+    fi
 done
